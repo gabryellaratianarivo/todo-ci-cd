5a87e68784e1d00172a86e3d3f46c056f5f30901
Gabryella Ratianarivo <60510279+gabryellaratianarivo@users.noreply.github.com>
2026-04-10 15:35:21 +0300

    Add files via upload

--- frontend/script.js
+++ frontend/script.js
@@ -3,8 +3,10 @@ const API = "http://localhost:5000/tasks";
 async function loadTasks() {
   const res = await fetch(API);
   const tasks = await res.json();
+
   const list = document.getElementById("tasks");
   list.innerHTML = "";
+
   tasks.forEach(t => {
     const li = document.createElement("li");
     li.textContent = t.title;
@@ -14,11 +16,15 @@ async function loadTasks() {
 
 async function addTask() {
   const input = document.getElementById("taskInput");
+
+  if (!input.value) return;
+
   await fetch(API, {
     method: "POST",
     headers: {"Content-Type": "application/json"},
     body: JSON.stringify({title: input.value})
   });
+
   input.value = "";
   loadTasks();
 }
